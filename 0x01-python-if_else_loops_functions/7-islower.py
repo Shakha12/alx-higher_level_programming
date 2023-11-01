b39f48dc9b2b022a93fba0084230a3e49f49fcaa
root <gicherushadrack@gmail.com>
2023-10-31 18:59:20 -0700

    7 islower2

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord("a") and or d(c) <= ord("z"):
-         return True
+    if ord(c) > 96:
+        return True
     else:
         return False
